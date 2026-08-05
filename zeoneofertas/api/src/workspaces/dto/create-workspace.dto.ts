f80e7b77bad56affb829f714d9c1442c505d710e
EzequielPires73 <ezequiel.pires082000@gmail.com>
2026-08-04 21:31:18 -0300

    commit

--- zeoneofertas/api/src/workspaces/dto/create-workspace.dto.ts
+++
@@ -1,8 +0,0 @@
-export class CreateWorkspaceDto {
-  name!: string;
-  slug?: string;
-  marketplaces?: string[];
-  socialNetworks?: string[];
-  categories?: string[];
-  workStructure?: string;
-}
